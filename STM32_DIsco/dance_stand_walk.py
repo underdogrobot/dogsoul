c0d29280df3cec05363e8712e29f78b0032bae2c
Frank G <frank@aioptusa.org>
2023-04-12 03:04:07 +0800

    Update dance_stand_walk.py

--- STM32_DIsco/dance_stand_walk.py
+++ STM32_DIsco/dance_stand_walk.py
@@ -1,4 +1,4 @@
-# This is a newer program that can dance, walk and stand
+# This is a newer program that can dance, walk and stand based on the mpr121 pins
 import time
 import board
 import pwmio
@@ -10,15 +10,6 @@ import adafruit_mpr121
 i2c = busio.I2C(board.PA08, board.PC09)
 mpr121 = adafruit_mpr121.MPR121(i2c)
 
-
-i = False
-
-while i == False:
-    print("Testing")
-    for i in range(12):
-        if mpr121[i].value:
-            print("Pin  touched!")
-            i = True
 
 
 # create a PWMOut object on Pin A2.
@@ -41,6 +32,11 @@ delay = 0.01
 
 def dance():
     while True:
+# when pin 2 touched, dance
+        for i in range(12):
+            if mpr121[i].value == True:
+                if != 2:
+                    break
         for angle in range(0, 180, 5):  # 0 - 180 degrees, 5 degrees at a time.
             my_servo1.angle = angle
             time.sleep(delay)
@@ -68,6 +64,7 @@ def stand():
     my_servo4.angle = 90
     
 def bow():
+# when pin 8 touched, bow
     for angle in range(0, 90, 3):  # 0 - 180 degrees, 5 degrees at a time.
         my_servo1.angle = 90 - angle
         time.sleep(delay)
@@ -81,6 +78,12 @@ def bow():
 
 def walk():
     while True:
+# when pin 6 touched, walk
+# Use touched_pins to get current state of all pins.
+        touched = mpr121.touched_pins
+# Test if 6 is touched.
+        if NOT touched[6]:
+            break
         for angle in range(0, 90, 3):  # 0 - 180 degrees, 5 degrees at a time.
             my_servo1.angle = 90 - angle
             time.sleep(delay)
@@ -92,15 +95,24 @@ def walk():
             time.sleep(delay)
             print("walking")
 
+stand()
+stopRobot = False
+
+while stopRobot == False:    
+    for i in range(12):      
+        if mpr121[i].value == True:
+            if i == 2:
+                dance()
+            elif i == 4:
+                stand()
+            elif i == 6:
+                walk()
+            elif i == 8:
+                bow()
+            else:
+                stopRobot = True 
+                break
 
 
-#stand()
 
 
-
-#dance()
-    
-# notes: 
-# use correct pins
-# use ground to reduce noise
-# need to change screw of one front leg
