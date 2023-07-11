75f564f26e55782109ea768b0da61efd52764dab
BrokenKeyboard <satasiyahet@gmail.com>
2023-07-10 20:16:11 -0700

    Create uart_hello.py

---
+++ pico/uart_hello.py
@@ -0,0 +1,18 @@
+from machine import UART,Pin
+import utime
+
+TX_PIN=16 # physical pin 21
+RX_PIN=17 # physical pin 22
+UART_NUM = 0 # Since pins 21 and 22 are part of the UART0
+BAUDRATE=9600
+PARITY_BITS=1
+BITS=8
+STOP=1
+
+
+
+uart = UART(UART_NUM,baudrate=BAUDRATE, tx=Pin(TX_PIN), rx=Pin(RX_PIN), bits=BITS, parity=PARITY_BITS, stop=STOP)
+val = uart.write(b"hello\r\n") # write some dummy text with a CRLF ends
+uart.flush() #required to ensure the write is finished
+rcvChar = uart.readline()# read the line (reads until it finds a valid line ending, in this case CRLF)
+print(rcvChar) # print the recieved info.
